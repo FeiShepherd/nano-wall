7b5bab4369fdaa29a16ab10e86803bf539c8921b
Feishepherd <thomasshepherd2002@gmail.com>
2019-04-09 09:42:23 +0700

    Add generator and tests

---
+++ generator/index.js
@@ -0,0 +1,24 @@
+let { client } = require("raiblocks-client")
+const fs = require("fs")
+const util = require("util")
+const writeFile = util.promisify(fs.writeFile)
+const rai_node_host = "http://[::1]:7076"
+const wallet =
+  "000D1BAEC8EC208142C99059B393051BAC8380F9B5A2E6B2489A277D81789F3F"
+
+const generate = async () => {
+  let raiClient = client({
+    rai_node_host
+  })
+  try {
+    addresses = await raiClient.accounts_create({
+      wallet,
+      count: 250000
+    })
+    console.log(addresses)
+  } catch (err) {
+    throw err
+  }
+}
+generate()
+module.exports = generate
---
+++ generator/index.test.js
@@ -0,0 +1,49 @@
+const assert = require("assert")
+const sinon = require("sinon")
+const proxyquire = require("proxyquire")
+
+describe("generator", () => {
+  let generate, raiClient, host, client, response
+  beforeEach(() => {
+    rai_node_host = "http://[::1]:7076"
+    response = {
+      accounts: [
+        "xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000",
+        "xrb_1e5aqegc1jb7qe964u4adzmcezyo6o146zb8hm6dft8tkp79za3s00000000"
+      ]
+    }
+    raiClient = {
+      accounts_create: sinon.stub().resolves(response)
+    }
+    client = sinon.stub().returns(raiClient)
+    generate = proxyquire("./index.js", {
+      "raiblocks-client": { client }
+    })
+  })
+  describe("#generate()", () => {
+    it("should call client with host", async () => {
+      await generate()
+      assert(client.calledWith({ rai_node_host }))
+    })
+    it("should call create account", async () => {
+      await generate()
+      assert(
+        raiClient.accounts_create.calledWith({
+          wallet: sinon.match.any,
+          count: 250000
+        })
+      )
+    })
+    it("should catch error", async () => {
+      let error
+      raiClient.accounts_create.rejects("test")
+      try {
+        await generate()
+      } catch (err) {
+        error = err
+      }
+      assert.equal(error.name, "test")
+    })
+    it("should store addresses into json file", () => {})
+  })
+})
