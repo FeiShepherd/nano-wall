33e0ceb30b5e43c423f5503577225e01a4134005
Feishepherd <thomasshepherd2002@gmail.com>
2019-04-21 13:00:50 +0700

    Set port in parameter in server start

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ const express = require("express")
 const routes = require("./routes/index.js")
 const bodyParser = require("body-parser")
 
-const start = () => {
+const start = (PORT = 3000) => {
   const app = express()
 
   app.use(routes)
   app.use(bodyParser)
-  app.listen(process.env.PORT || 3000, () =>
-    console.log(`Example app listening on port ${process.env.PORT || 3000}!`)
+  app.listen(process.env.PORT || PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT || PORT}!`)
   )
 }
 
