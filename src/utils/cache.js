2042b8da25114422766f76cf9ed3c2c084e067dd
Feishepherd <thomasshepherd2002@gmail.com>
2019-04-21 16:36:02 +0700

    Add new caching module for pixelHandle

---
+++ src/utils/cache.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const fs = require('fs')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
+const path = require('path')
+
+module.exports = (function() {
+  let pixels = {}
+  return {
+    get: () => {
+      return pixels
+    },
+    init: async () => {
+      const contents = await readFile(
+        path.join(__dirname, './addresses.json'),
+        'utf8'
+      )
+      pixels = JSON.parse(contents)
+      return pixels
+    },
+    set: (receiver, sender) => {
+      pixels[receiver] = sender
+    },
+    addressExist: address => {
+      return address in pixels
+    }
+  }
+})()
