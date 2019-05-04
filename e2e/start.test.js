c1637b891d11f711ad3f4f55b700e05fcd8d0415
Feishepherd <thomasshepherd2002@gmail.com>
2019-05-04 20:29:54 +0700

    Finish block validate

--- e2e/start.test.js
+++ e2e/start.test.js
@@ -2,7 +2,9 @@ const request = require('supertest')
 const block = require('./sample-block.js')
 const server = require('../src/server.js')
 const Readable = require('stream').Readable
+
 process.env.NODE_ENV = 'development'
+process.env.RAI_NODE_HOST = 'http://[::1]:7076'
 
 describe('lel', () => {
   it('lel2', async () => {
